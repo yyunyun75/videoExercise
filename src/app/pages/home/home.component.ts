0d9316da46718dfd07e30b54a0d0394422edf438
Leslie Lee <leslie@krossover.com>
2017-06-11 18:15:38 -0400

    init css and layout

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -7,9 +7,49 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+    emojis = [
+        'em-angry',
+        'em-blush',
+        'em-confused',
+        'em-cry',
+        'em-disappointed',
+        'em-dizzy_face',
+        'em-expressionless',
+        'em-flushed',
+        'em-grinning',
+        'em-kissing',
+        'em-relaxed',
+        'em-smile'
+    ];
 
-  ngOnInit() {
-  }
+    videos = [
+        {'showActions': false, 'playing': false},
+        {'showActions': false, 'playing': false},
+        {'showActions': false, 'playing': false},
+        {'showActions': false, 'playing': false},
+        {'showActions': false, 'playing': false},
+        {'showActions': false, 'playing': false},
+        {'showActions': false, 'playing': false},
+        {'showActions': false, 'playing': false},
+        {'showActions': false, 'playing': false},
+        {'showActions': false, 'playing': false}
+    ];
+
+    constructor() { }
+
+    ngOnInit() {
+    }
+
+    toggleEmoji(indx){
+        if(this.videos[indx] && this.videos[indx].showActions){
+            this.videos[indx].showActions = false;
+        }else{
+            this.videos[indx].showActions = true;
+        }
+    }
+
+    playVideo(indx){
+        this.videos[indx].playing = true;
+    }
 
 }
